fdb2940aef9c5ffa9646fb13db36e0627c3c5be8
Suyeon Kang <suyeon.dev@gmail.com>
2023-06-18 14:55:36 +0900

    chore(AppModule): Implement TypeORM with sqlite

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { InterviewModule } from './interview/interview.module';
 
 @Module({
-  imports: [InterviewModule],
-  controllers: [],
-  providers: [],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'sqlite',
+            database: 'development.db',
+            entities: [],
+            synchronize: true
+        }),
+        InterviewModule
+    ],
+    controllers: [],
+    providers: []
 })
 export class AppModule {}
