328fe62fb9556337d08b877aaa0622c7f563ae3d
Suyeon Kang <suyeon.dev@gmail.com>
2024-06-12 16:50:16 +0900

    feat(frontend/next.config.mjs): remove console on production mode

--- packages/frontend/next.config.mjs
+++ packages/frontend/next.config.mjs
@@ -1,7 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  swcMinify: true, // JavaScript 코드를 압축
-  optimizeFonts: true,
+  swcMinify: true,
+  reactStrictMode: true,
+  // generateBuildId: async () => {
+  //   next build시, 어플리케이션 버전 명시
+  //   return;
+  // },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  experimental: {
+    // 용량이 큰 패키지 명시하면 실제 사용하는 모듈만 로딩하여 최적화
+    optimizePackageImports: [],
+  },
 };
+
+if (process.env.NEXT_PUBLIC_NODE_ENV === "production") {
+  nextConfig.compiler = {
+    removeConsole: {
+      exclude: ["error", "warn"],
+    },
+  };
+}
 
 export default nextConfig;
