97db90b80570d83134b5d5627e8287542612c80b
Suyeon Kang <suyeon.dev@gmail.com>
2024-06-14 14:42:57 +0900

    chore(frontend/api): init react-query for fetching data

--- packages/frontend/src/app/layout.tsx
+++ packages/frontend/src/app/layout.tsx
@@ -1,28 +1,33 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 
 import "./globals.css";
 import { Navigation } from "@components";
 import NAVIGATION from "@constants/navigation";
+import ReactQueryProvider from "@api/utils/react-query/ReactQueryProvider";
 
 export const metadata: Metadata = {
   title: "Tell me about your career",
   description: "A simple platform where people can explore various jobs.",
 };
 
-export default function RootLayout({
+const RootLayout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>) => {
   return (
     <html lang="en">
       <body suppressHydrationWarning>
         <header>
           <Navigation links={NAVIGATION} logoSrc="/logo-2.svg" />
         </header>
-        <main>{children}</main>
+
+        <ReactQueryProvider>
+          <main>{children}</main>
+        </ReactQueryProvider>
       </body>
     </html>
   );
-}
+};
+
+export default RootLayout;
